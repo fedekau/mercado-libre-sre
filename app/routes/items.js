c74f47f092397badffc8b11ddd78c6ef6a7c906e
Federico Kauffman <federico@wyeworks.com>
2017-12-01 21:41:58 -0300

    Use item gateway to return item data

--- app/routes/items.js
+++ app/routes/items.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const Router = express.Router();
+const ItemGateway = require('../services/item-gateway');
 
-Router.get('/:id', (request, response) => {
+Router.get('/:id', async (request, response) => {
   var id = request.params.id;
 
-  response.json({ id });
+  const item = await ItemGateway.get(id);
+
+  response.json(item);
 });
 
 module.exports = Router;
