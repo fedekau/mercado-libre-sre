9332c6f7ffbaf86b0da7ea238f1e8c5049cda5c3
Federico Kauffman <federico@wyeworks.com>
2017-12-02 18:16:41 -0300

    Add morgan for logging

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ dotenv.config();
 
 const application = require('./app/index');
 const express = require('express');
+const morgan = require('morgan');
+
 const server = express();
 
+server.use(morgan('tiny'));
 server.use(application);
 
 server.listen(3000);
