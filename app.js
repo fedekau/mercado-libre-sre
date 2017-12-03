b9ee6ea9c63e908ed81dbe64da174731ed3d1f8d
Federico Kauffman <federico@wyeworks.com>
2017-12-02 23:43:26 -0300

    Add metric avg_response_time

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const responseTime = require('response-time')
 const application = require('./app/index');
 const express = require('express');
 const morgan = require('morgan');
 
 const server = express();
+
+const MetricsPublisher = require('./app/services/metrics-publisher');
+const MetricNames = require('./lib/metrics/metric-names');
+
+server.use(responseTime(function (req, res, time) {
+  MetricsPublisher.publish(MetricNames.response_time, time);
+}));
 
 server.use(morgan('tiny'));
 server.use(application);
