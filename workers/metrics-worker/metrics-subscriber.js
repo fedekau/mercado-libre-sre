655c53a9c7eadddfedfd59039ee3fd403851190f
Federico Kauffman <federico@wyeworks.com>
2017-12-03 08:06:10 -0300

    Fix: Remove multiple subscriptions to same event

--- workers/metrics-worker/metrics-subscriber.js
+++ workers/metrics-worker/metrics-subscriber.js
@@ -2,14 +2,13 @@ const redis = require('redis');
 const sub = redis.createClient();
 const MetricsCruncher = require('./metrics-cruncher');
 
+sub.on("message", function (metricName, rawMessage) {
+  const message = JSON.parse(rawMessage);
+
+  MetricsCruncher.crunch(metricName, message);
+});
 class MetricsSubscriber {
   static subscribe(metricName) {
-    sub.on("message", function (metricName, rawMessage) {
-      const message = JSON.parse(rawMessage);
-
-      MetricsCruncher.crunch(metricName, message);
-    });
-
     sub.subscribe(metricName);
   }
 }
